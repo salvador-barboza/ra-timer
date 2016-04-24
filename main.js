fe2880accf1e6e07d6be679229b06ee79775e8e0
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2016-04-23 19:51:23 -0600

    Add sound and UI improvements

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 200, height: 200 /*, resizable: false*/});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
